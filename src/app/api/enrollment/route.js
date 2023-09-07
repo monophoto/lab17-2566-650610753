9e7c507e2240bdae385bf8cc7bb0f418258431d1
monophoto <jetsadawut1013@gmail.com>
2023-09-07 16:21:45 +0700

    completed!

--- src/app/api/enrollment/route.js
+++ src/app/api/enrollment/route.js
@@ -28,13 +28,14 @@ export const GET = async (request) => {
   //check if user provide one of 'studentId' or 'courseNo'
   //User must not provide both values, and must not provide nothing
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Please provide either studentId or courseNo and not both!",
-  //   },
-  //   { status: 400 }
-  // );
+  if ((studentId && courseNo) || (!studentId && !courseNo))
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Please provide either studentId or courseNo and not both!",
+      },
+      { status: 400 }
+    );
 
   //get all courses enrolled by a student
   if (studentId) {
@@ -60,10 +61,17 @@ export const GET = async (request) => {
     const studentIdList = [];
     for (const enroll of DB.enrollments) {
       //your code here
+      if (enroll.courseNo === courseNo) {
+        studentIdList.push(enroll.studentId);
+      }
     }
 
     const students = [];
     //your code here
+    for (const studentId of studentIdList) {
+      const student = DB.students.find((x) => x.studentId === studentId);
+      students.push(student);
+    }
 
     return NextResponse.json({
       ok: true,
@@ -141,16 +149,21 @@ export const DELETE = async (request) => {
   const { studentId, courseNo } = body;
 
   //check if studentId and courseNo exist on enrollment
+  const foundIndex = DB.enrollments.findIndex(
+    (fid) => fid.studentId === studentId && fid.courseNo === courseNo
+  );
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Enrollment does not exist",
-  //   },
-  //   { status: 404 }
-  // );
+  if (foundIndex === -1)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Enrollment does not exist",
+      },
+      { status: 404 }
+    );
 
   //perform deletion by using splice or array filter
+  DB.enrollments.splice(foundIndex, 1);
 
   //if code reach here it means deletion is complete
   return NextResponse.json({
